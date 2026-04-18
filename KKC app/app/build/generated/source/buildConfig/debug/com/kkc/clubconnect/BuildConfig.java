2b19e03926440ea2cf0cbb7ae3175a61af349f4e
YESHWANTH. V. <vyeshwanth0621@gmail.com>
2026-04-18 09:26:10 +0530

    first commit

---
+++ KKC app/app/build/generated/source/buildConfig/debug/com/kkc/clubconnect/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kkc.clubconnect;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.kkc.clubconnect";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+  // Field from default config.
+  public static final String SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1dmNmcm1mcXBvc2twanhtemVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5MzM0MDIsImV4cCI6MjA5MDUwOTQwMn0.5qCzDf4ImVGZPav0vqd4xA2F8X29ehpHL1_IhsgZp6w";
+  // Field from default config.
+  public static final String SUPABASE_URL = "https://euvcfrmfqposkpjxmzej.supabase.co";
+}
